1c11b6950e86899d77b4945a3699646fef79f1e7
Gromov Anton <antongromov@mail.ru>
2019-10-17 21:23:54 +0700

    backend for app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into Person
+values(1,'Громов Антон Викторович');
+
+insert into Language
+values(2001,'Java',1);
+insert into Language
+values(2002,'JS',1);
+insert into Language
+values(2003,'Groovy',1);
+
+insert into Interest
+values(1001,'All intresting',1);
