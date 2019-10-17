780f2b79393cea47a17afb8d2f2d421ea3ae9524
Gromov Anton <antongromov@mail.ru>
2019-10-17 20:20:17 +0700

    Initial commit

---
+++ src/main/java/com/example/personcard/PersoncardApplication.java
@@ -0,0 +1,13 @@
+package com.example.personcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoncardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersoncardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/personcard/PersoncardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoncardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
